a8be7290d56c7d909ecb33998b5872418145519d
ianhu92 <ianhu92@gmail.com>
2015-12-03 19:16:35 -0500

    index.html add link to personal.html

--- final/poll/js/poll.js
+++ final/poll/js/poll.js
@@ -1,5 +1,5 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
+    width = (window.innerWidth - 20) / 2 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var color = d3.scale.category20();
@@ -10,15 +10,22 @@ var y = d3.scale.linear()
     .range([height, 0]);
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .ticks(d3.time.month, 1)
+    .tickFormat(d3.time.format("%B"));
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
+/*var candidates = {
+ dem: ["MoE", "Clinton", "Sanders", "O'Malley"],
+ rep: ["Trump", "Carson", "Rubio", "Cruz", "Bush", "Paul", "Kasich", "Fiorina", "Huckabee", "Christie", "Jindal", "Santorum", "Pataki", "Graham"]
+ };*/
 var candidates = {
-    dem: ["MoE", "Clinton", "Sanders", "O'Malley"],
-    rep: ["Trump", "Carson", "Rubio", "Cruz", "Bush", "Paul", "Kasich", "Fiorina", "Huckabee", "Christie", "Jindal", "Santorum", "Pataki", "Graham"]
-};
+    dem: ["Clinton", "Sanders"],
+    rep: ["Trump", "Carson", "Rubio", "Cruz", "Bush"]
+};// filter some candidates
+var months = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
 var draw = function (party) {
     var svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -40,12 +47,21 @@ var draw = function (party) {
                 return y(d.poll);
             });
 
-        data.forEach(function (d) {// process data
+        // filter data with Array.prototype.filter
+        var baseDate = new Date(2015, 6, 1);// July 1, 2015
+        data = data.filter(function (d) {
+            return new Date(dateParser(d.date)) >= baseDate;
+        });
+
+        // process data
+        data.forEach(function (d, i) {
             d.date = dateParser(d.date);
             for (var key in candidates[party]) {
                 d[candidates[party][key]] = +d[candidates[party][key]];
             }
         });
+
+        console.log(data);
 
         // set axis range
         x.domain(d3.extent(data, function (d) {
@@ -123,27 +139,53 @@ var draw = function (party) {
             .data(data)
             .enter()
             .append("g")
-            .attr("class", "event");
+            .attr("class", "event")
+            .attr("description", function (d) {
+                return d.event;
+            })
+            .attr("date", function (d) {
+                var date = new Date(d.date);
+                var yyyy = date.getFullYear().toString();
+                var mm = date.getMonth(); // getMonth() is zero-based
+                var dd = date.getDate().toString();
+                mm = months[mm];
+                return mm + " " + dd + ", " + yyyy;
+            });
         gEvent.append("line")
             .attr("class", "event_line")
             .attr("x1", function (d) {
-                return x(new Date(d.date));
+                return parseInt(x(new Date(d.date)));
             })
             .attr("y1", 0)
             .attr("x2", function (d) {
-                return x(new Date(d.date));
+                return parseInt(x(new Date(d.date)));
             })
             .attr("y2", height)
-            .attr("stroke", "#000");
+            .attr("stroke", "#000")
+            .attr("stroke-width", "2px");
     });
 };
 var bindEvent = function () {
     $("svg").on("mouseenter", ".path", function (e) {
-        $("#mouseTip").css("display", "inline-block").css("left", e.clientX + 3).css("top", e.clientY);
-        $("#mouseTipName").text($(this).attr("name"));
+        $("#candidateTip").css("display", "block").css("left", e.clientX).css("top", e.clientY);
+        $("#candidateTipName").text($(this).attr("name"));
     }).on("mouseleave", ".path", function () {
-        $("#mouseTip").css("display", "none");
+        $("#candidateTip").css("display", "none");
     });
+
+    $("svg").on("mouseenter", ".event", function (e) {
+        $("#eventTip").css("display", "block").css("left", e.clientX).css("top", e.clientY - 10);
+        $("#eventTipDate").text($(this).attr("date"));
+        $("#eventTipDescription").text($(this).attr("description"));
+    }).on("mouseleave", ".event", function (e) {
+        $("#eventTip").css("display", "none");
+    });
+
+    $("#candidateTip,#eventTip").on("mouseenter", function () {
+        $(this).css("display", "block");
+    }).on("mouseleave", function () {
+        $(this).css("display", "none");
+    })
 };
 
 // call funcitons
