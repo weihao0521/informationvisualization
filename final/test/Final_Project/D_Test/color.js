19c316d0820f14ef73bd43ba68c2e36dc35e0a8c
dingran556 <dingran556@gmail.com>
2015-12-05 15:07:02 -0500

    Small Update

--- final/test/Final_Project/D_Test/color.js
+++ final/test/Final_Project/D_Test/color.js
@@ -2,26 +2,41 @@
  * Created by Ian on 9/29/2015.
  */
 var color = (function () {
-    var color = ["#fcc5c0", "#fa9fb5", "#f768a1", "#dd3497", "#ae017e", "#7a0177"];
-    var range = ["45% - 50%", "50% - 55%", "55% - 60%", "60% - 65%", "65% - 70%", "> 70%"];
+    var color = ["#F8F9D0","#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#034e7b"];
+    var color_swing = ["#F8F9D0","#fdd49e", "#fc8d59", "#ef6548", "#d7301f", "#d7301f", "#990000"];
+    /*var H_range = [" No Data Available", " 45% - 50%", " 50% - 55%", " 55% - 60%", " 60% - 65%", " 65% - 70%","Above 70%"];
+    var S_range = [" No Data Available", " 10% - 15%", " 15% - 20%", " 20% - 30%", " 30% - 40%", " 40% - 50%"];*/
+
+    var range= {
+        Hillary: [45, 50, 55, 60, 65, 70],
+        Sanders: [10, 15, 20, 30, 40, 50]
+    }
     return {
-        getFillKey: function (percent) {
-            if (percent >= 45 && percent <= 50) {
-                return "45";
-            } else if (percent <= 55) {
-                return "50";
-            } else if (percent <= 60) {
-                return "55";
-            } else if (percent <= 65) {
-                return "60";
-            } else if (percent <= 70) {
-                return "65";
-            } else {
-                return "70";
+        getFillKey: function (percent, candidate) {
+            var rangeArray = range[candidate];
+            for(var index = 0 ;index<rangeArray.length;index++){
+                if(percent<=rangeArray[index]){
+                    return index;
+                }
             }
         },
-        getColor: function (index) {
-            return color[index];
+        getFillKey2: function (percent) {
+            if (percent >= 45 && percent <= 50) {
+                return "46";
+            } else if (percent <= 55) {
+                return "51";
+            } else if (percent <= 60) {
+                return "56";
+            } else if (percent <= 65) {
+                return "61";
+            } else if (percent <= 70) {
+                return "66";
+            } else {
+                return "71";
+            }
+        },
+        getColor: function (index,isSwing) {
+            return isSwing?color_swing[index]:color[index];
         },
         getRange: function(index){
             return range[index];
