946f93cf5256834657c0f2290d4c7e9edd54997c
ianhu92 <ianhu92@gmail.com>
2015-12-08 22:09:03 -0500

    super pac cap text move right

--- final/js/superpac/superpac.js
+++ final/js/superpac/superpac.js
@@ -92,7 +92,7 @@
             svg.append("text")
                 .attr("class", "cap text")
                 .attr("transform", function () {
-                    return party == "dem" ? "translate(" + (width - 300) + "," + (y(90717000) + 18) + ")" : "translate(" + (width - 300) + "," + (y(33717000) + 18) + ")";
+                    return party == "dem" ? "translate(" + (width - 270) + "," + (y(90717000) + 18) + ")" : "translate(" + (width - 270) + "," + (y(33717000) + 18) + ")";
                 })
                 .attr("dy", ".35em")
                 .attr("text-anchor", "start")
