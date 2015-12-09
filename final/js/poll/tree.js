314e5f5a03b8ae89e62ea907ef92ee41b0d453e1
ianhu92 <ianhu92@gmail.com>
2015-12-08 22:59:36 -0500

    tree text position change

--- final/js/poll/tree.js
+++ final/js/poll/tree.js
@@ -37,7 +37,7 @@
             // .attr("transform", "translate(" + m[3] + "," + m[0] + ")"); // left-right
             // .attr("transform", "translate(" + m[0] + "," + m[3] + ")"); // top-bottom
             //.attr("transform", "translate(0,"+h/2+")"); // bidirectional-tree
-            .attr("transform", "translate(20,15)");
+            .attr("transform", "translate(20,24)");
 
 
         var that = {
